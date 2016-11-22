50f0ed8e64a35c93d3993fcfe0219c2fd8860e0f
Your Name <you@example.com>
2016-11-22 16:57:01 +0700

    tgs1layla

---
+++ app/src/main/java/intermediate/tugas2/secondapp/Linear.java
@@ -0,0 +1,13 @@
+package intermediate.tugas2.secondapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Linear extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear);
+    }
+}
